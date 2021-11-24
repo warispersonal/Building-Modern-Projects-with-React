516a78518d79a1fd8b5fabdabd9a46300298471e
m_waris <mwa@eventbuizz.com>
2021-11-24 16:52:55 +0500

    react & react-dom setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css'
+
+const App = () =>{
+    return(
+        <div className="App">
+            <h1>App Component</h1>
+        </div>
+    )
+}
+export default App;
