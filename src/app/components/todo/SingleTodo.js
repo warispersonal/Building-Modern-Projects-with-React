26ebeaddf3c536bcafd9fd7b7381f89cf599f27f
warispersonal <wariszargarpersonal@gmail.com>
2021-12-15 08:42:35 +0500

    Add redux-thunk

--- src/app/components/todo/SingleTodo.js
+++ src/app/components/todo/SingleTodo.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {removeTodo} from '../../../store/action/todo'
+import {showAlert} from "@/todo/todoThunk";
 
-const SingleTodo = ({todo, removeTodo}) =>{
+const SingleTodo = ({todo, removeTodo, showAlert}) =>{
     const {text, isComplete} = todo
     return(
         <div style={{border: "2px solid black", margin:'10px', padding:"25px"}}>
@@ -11,6 +12,8 @@ const SingleTodo = ({todo, removeTodo}) =>{
                 {isComplete ? "" : <button>Mark as complete</button>}
 
                 <button onClick={()=>removeTodo(text)}>Remove</button>
+
+                <button onClick={()=>showAlert(text)}>Show Alert</button>
             </div>
         </div>
     )
@@ -18,7 +21,8 @@ const SingleTodo = ({todo, removeTodo}) =>{
 
 const mapDispatchToProps = (dispatch) =>{
     return{
-        removeTodo: text => dispatch(removeTodo(text))
+        removeTodo: text => dispatch(removeTodo(text)),
+        showAlert: text => dispatch(showAlert(text))
     }
 }
 
